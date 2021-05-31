6ff821f6681446e81cd440813a51acf20d1daaed
melander2910 <58288414+melander2910@users.noreply.github.com>
2021-05-31 14:23:32 +0200

    Update style.js

--- style.js
+++ style.js
@@ -2,6 +2,6 @@ const theme = document.getElementById("theme");
 
 theme.addEventListener("change", () => {
     document.body.classList.toggle("dark");
-    document.getElementsByClassName("drumContainer").classList.toggle("dark")
+    //document.getElementsByClassName("drumContainer").classList.toggle("dark")
 });
 
